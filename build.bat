8cca30fd97dcebf717ca64e180c438053f8de342
GatoArtStudio <hackhcctecno@gmail.com>
2024-04-28 23:34:55 -0500

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,1 @@
+flet build windows --build-number 1 --build-version "1.0" --product "GatoDeck" --copyright "Github GatoDeck" --description "Aplicacion de GatoDeck para escritorio" --company "By GatoArtStudio"
